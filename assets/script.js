ed7c98a6e0889cfc259ff6288603ac20d83cbf16
Diana Tran <diana.tran44@gmail.com>
2023-11-25 17:21:18 +1100

    updated code for menu

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
+var day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+var breakfastCategory = 'Breakfast';
+var userCategory = 'Vegan';
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    // Function to create and append meal details to the specified menu
+        
     function addMealToMenu(day, mealType, meal) {
         var mealName = meal.strMeal;
         var mealThumbnail = meal.strMealThumb;
@@ -7,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var ingredients = [];
 
         // Collect ingredients and measurements from the meal object
-        for (var i = 1; i <= 50; i++) {
+        for (var i = 1; i <= 20; i++) {
             var ingredient = meal['strIngredient' + i];
             var measurement = meal['strMeasure' + i];
             if (ingredient && ingredient.trim() !== '') {
@@ -29,22 +34,32 @@ document.addEventListener('DOMContentLoaded', function() {
         var mealTitle = document.createElement('h6');
         mealTitle.textContent = mealName;
 
-        
+        var mealIngredientsEl = document.createElement('ul');
+        ingredients.forEach(ingredient => {
+            var li = document.createElement('li');
+            li.textContent = ingredient;
+            mealIngredientsEl.appendChild(li);
+
+        var mealInstructionsEl = document.createElement('p');
+        mealInstructionsEl.textContent = mealInstructions;
+      
+        });
+
         // Append elements to the specified menu based on the day and meal type
-        var menuId = `${day.toLowerCase()}-${mealType}`;
+        var menuId = `${day.toLowerCase()}-${mealType.toLowerCase()}`;
         var menu = document.getElementById(menuId);
         if (menu) {
             mealContainer.appendChild(mealImage);
             mealContainer.appendChild(mealTitle);
+            mealContainer.appendChild(mealIngredientsEl);
+            mealContainer.appendChild(mealInstructionsEl);
             menu.appendChild(mealContainer);
         }
     }
-    
-var category = "Vegan";
-var area = "Chinese";
 
-function getRandomMealByCategoryAndArea(category, area) {
-    let apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}&a=${area}`;
+    // Function to fetch meal details by category
+function getMealDetailsByCategory(category) {
+    let apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
 
     return fetch(apiUrl)
         .then(response => {
@@ -57,67 +72,63 @@ function getRandomMealByCategoryAndArea(category, area) {
             if (data.meals && data.meals.length > 0) {
                 // Select a random meal from the fetched list
                 const randomIndex = Math.floor(Math.random() * data.meals.length);
-                return data.meals[randomIndex];
-                const mealId = randomMeal.idMeal; // Extract meal ID
+                const randomMeal = data.meals[randomIndex]; // Get a random meal
 
-                // Return meal details along with mealId
-                return { meal: randomMeal, mealId: mealId };
-         } else {
-             return { meal: null, mealId: null }; // Return null values if no meals found
-         }
+                // Extract the meal ID of the random meal
+                const mealId = randomMeal.idMeal;
+                return mealId;
+            } else {
+                return null; // Return null if no meals found
+            }
+        })
+        .then(mealId => {
+            if (mealId) {
+                // Fetch meal details by ID
+                let mealDetailsUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+                return fetch(mealDetailsUrl)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error(`Network response was not ok: ${response.status}`);
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        if (data.meals && data.meals.length > 0) {
+                            // Return the details of the meal
+                            const mealDetails = data.meals[0];
+                            return mealDetails;
+                        } else {
+                            return null; // Return null if no meal details found
+                        }
+                    });
+            } else {
+                return null; // Return null if no meal ID available
+            }
         })
         .catch(error => {
-            console.error('There was a problem fetching the data:', error);
+            console.error('Error fetching meal details:', error);
             return null;
         });
 }
+    // Function to fetch a meal for a specific day and meal type
+    function fetchAndDisplayMeal(day, mealType, category) {
+        getMealDetailsByCategory(category)
+            .then(randomMeal => {
+                if (randomMeal) {
+                    addMealToMenu(day, mealType, randomMeal);
+                } else {
+                    console.log(`No ${mealType.toLowerCase()} found for ${day}`);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching meal:', error);
+            });
+    
+        }
 
-// Function to fetch a meal for a specific day and meal type
-function fetchAndDisplayMeal(day, mealType) {
-    getRandomMealByCategoryAndArea(category, area)
-        .then(randomMeal => {
-            if (randomMeal) {
-                // Call addMealToMenu for the random meal fetched
-                addMealToMenu(day, mealType, randomMeal);
-            } else {
-                console.log("No meals found with the provided criteria.");
-            }
-        });
-}
-
-// Iterate through days (Monday to Friday) and fetch a random meal for each meal type
-var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-
-days.forEach(day => {
-    fetchAndDisplayMeal(day, 'lunch');
-    fetchAndDisplayMeal(day, 'dinner');
+    day.forEach(day => {
+        fetchAndDisplayMeal(day, 'Lunch', userCategory);
+        fetchAndDisplayMeal(day, 'Dinner', userCategory);
+        fetchAndDisplayMeal(day, 'Breakfast', breakfastCategory);
+    });
 });
-
-var breakfastCategory = "Breakfast";
-
-// Function to fetch a breakfast meal for a specific day
-function fetchAndDisplayBreakfast(day) {
-    getRandomMealByCategoryAndArea(breakfastCategory, area)
-        .then(randomMeal => {
-            if (randomMeal) {
-                // Call addMealToMenu for the breakfast meal fetched
-                addMealToMenu(day, 'breakfast', randomMeal);
-            } else {
-                console.log("No meals found with the provided criteria for breakfast.");
-            }
-        });
-}
-
-// Iterate through days (Monday to Friday) and fetch a breakfast meal for each day
-days.forEach(day => {
-    fetchAndDisplayBreakfast(day);
-
-
-}); 
-
-
-
-
-});
-
-
